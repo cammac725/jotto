7ddef0fcac386f38d73d0f2bc5bd3caa9942b965
camm@rocktechcomputer.com <camm@rocktechcomputer.com>
2019-06-03 21:20:19 -0700

    added Congrats tests

---
+++ src/Congrats.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/** 
+ * Functional react component for congratulatory message
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null
+ *  if 'success' props is false)
+*/
+
+export default () => {
+  return (
+    <div>
+    
+    </div>
+  )
+}
